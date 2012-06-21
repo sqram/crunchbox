fa11e29a98605462a1b40554bdc6fad637c5ba62
lyrae <lyrae@nowhere.home>
2012-06-21 19:19:02 -0400

    small change to about dialog. changed openbox command to restart instead of reload

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
+Ignore this file.
+To run crunchbox, just do
+$ python /path/to/crunchbox/src/crunchbox.py
+(you might have to do $ python2 if your system loads python3 by default)
 """
+#!/usr/bin/env python
 from distutils.core import setup
 
 setup(name='crunchbox',
