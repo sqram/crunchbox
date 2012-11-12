1191c9ac2fa5bb4360550c234d99160a707f031b
katsh <bruce83@gmail.com>
2012-11-12 11:26:07 -0500

    added scrollbars (scrolledwindow)

--- src/dialog_about.py
+++ src/dialog_about.py
@@ -9,20 +9,20 @@ in Brunch when the user clicks 'ok'
 import gtk
 
 class AboutDialog(gtk.AboutDialog):
-	
+
 	def __init__(self, brunch):
 		gtk.AboutDialog.__init__(self)
-		
+
 		self.set_program_name("Crunchbox")
 
-		self.set_website("http://none.io")
-		self.set_copyright("noneio")
+		self.set_website("http://kat.sh")
+		#self.set_copyright("katsh")
 		self.set_comments("A multiple themes switcher")
 		self.set_license("GNU GENERAL PUBLIC LICENSE")
-		self.set_authors(['Bruce Franchi (noneio)'])
+		self.set_authors(['katsh'])
 		self.set_artists(['No one. Looking for someone to design a logo.'])
-		
+
 
 		self.run()
 		self.destroy()
-	
+
--- src/gui.py
+++ src/gui.py
@@ -14,55 +14,59 @@ CONFIG_PATH = expanduser("~/.config/crunchbox/")
 
 
 class Gui:
-	
+
 	def __init__(self, crunchbox):
 		self.crunchbox = crunchbox
-		
+
 		# -- our window
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.set_border_width(10)
-		self.window.set_default_size(300, 50)
+		self.window.set_default_size(677, 350)
 		self.window.set_title("crunchbox")
-		
+
 		# -- create widgets
 		self.vbox_main = gtk.VBox()
 		img = gtk.Image()
-		
-		hbox_top_buttons = gtk.HBox()		
+		self.sw = gtk.ScrolledWindow()
+		hbox_top_buttons = gtk.HBox()
+		self.vbox_middle = gtk.VBox()
 		button_save = gtk.Button(stock=gtk.STOCK_SAVE)
 		button_about = gtk.Button(stock=gtk.STOCK_ABOUT)
 
 		self.tooltip = gtk.Tooltips()
-		
-		
+
+		# -- Widget properties
+		self.sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
 		# -- arrange layout
 		hbox_top_buttons.pack_start(button_save)
 		hbox_top_buttons.pack_start(button_about)
 		self.vbox_main.pack_start(hbox_top_buttons,0 ,0, 0)
-
+		self.sw.add_with_viewport(self.vbox_middle)
+		self.vbox_main.pack_start(self.sw, 1, 1, 1)
 		# -- call start up method(s)
 		self.load_screenshots()
-			
+
 		self.window.add(self.vbox_main)
-		
+
 		# -- event binding
 		self.window.connect('destroy', lambda x: gtk.main_quit())
 		button_save.connect("button_press_event", crunchbox.save_clicked)
 		button_about.connect("button_press_event", crunchbox.about_clicked)
-		
+
 		self.window.show_all()
 
-	
+
 	def append_screenshot(self, name):
 		'''appends screenshot to list when profile is saved'''
 		image = self._create_image(self.crunchbox.cb_screenshots_dir, name + '.jpg')
-		
+
 		hbox = self.boxlist[-1]
 		num_children =  len(hbox.get_children())
 		if num_children in [0,3]:
 			hbox = gtk.HBox()
 			hbox.pack_start(image)
-			self.vbox_main.add(hbox)
+			self.vbox_vbox_middle.add(hbox)
 			hbox.show()
 			self.boxlist.append(hbox)
 		else:
@@ -71,7 +75,7 @@ class Gui:
 
 
 
-		
+
 	def _create_image(self, path, f):
 		'''creates button with screenshot inside and return it'''
 		image = gtk.Image()
@@ -103,7 +107,7 @@ class Gui:
 		# -- load our screenshots from the cfg directory
 		# We want each row to hold 3 images. If an HBox has 3 children,
 		# create a new HBox and append the upcoming image to it.
-		
+
 		# a list containing hboxes; start with one inside
 		self.boxlist = [gtk.HBox()]
 		screenshots = os.listdir(self.crunchbox.cb_screenshots_dir)
@@ -111,11 +115,11 @@ class Gui:
 			image = self._create_image(self.crunchbox.cb_screenshots_dir, f)
 			if len(self.boxlist[-1].get_children()) == 3:
 				self.boxlist.append(gtk.HBox())
-			
+
 			self.boxlist[-1].pack_start(image)
 		# sacrificing a for loop here for one liner neatness.
-		map(lambda box: self.vbox_main.pack_start(box,0,0,0), self.boxlist)
-		
+		map(lambda box: self.vbox_middle.pack_start(box,0,0,0), self.boxlist)
+
 	def rightclick_menu(self, e, name, button):
 		menu = gtk.Menu()
 		item_delete = gtk.MenuItem('delete from crunchbox')
@@ -125,5 +129,5 @@ class Gui:
 		# we send the button widget because after we delete this profile, we remove it from the gui
 		item_delete.connect('activate', self.crunchbox.io.delete_profile, name, button)
 
-		
-		
+
+
