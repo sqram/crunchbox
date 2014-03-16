4ac7e1d82a98062400ec58ab784520003c6a9aa9
katsh <bruce83@gmail.com>
2014-03-16 19:26:02 -0400

    spectrwm plugin

--- src/plugins/spectrwm.py
+++ src/plugins/spectrwm.py
@@ -2,11 +2,11 @@ from shutil import copyfile
 from os.path import expanduser
 
 class Spectrwm:
-	
-	def __init__(self, io):
-		self.io = io
+
+	def __init__(self, base):
+		self.base = base
 		self.cfg = [expanduser('~/.spectrwm.conf')]
-	
+
 
 		# This is the path to where crunchbox stores all this plugin's cfg
 		self.class_name = self.__class__.__name__
@@ -14,9 +14,9 @@ class Spectrwm:
 
 
 	def save(self, profile_name, plugin_obj):
-		self.io.save(profile_name, self.class_name, plugin_obj)
+		self.base.save(profile_name, self.class_name, plugin_obj)
 
 	def load(self, profile_name, plugin_obj):
 		# conky restarts automatically when
 		# .conkyrc is changed. No need for special call()
-		self.io.load(profile_name, self.class_name, plugin_obj)
+		self.base.load(profile_name, self.class_name, plugin_obj)
--- src/plugins/weechat.py
+++ src/plugins/weechat.py
@@ -2,14 +2,13 @@ from subprocess import call
 from os.path import expanduser
 
 class Weechat:
-	
+
 	def __init__(self, base):
 		self.base = base
 		self.cfg = [
 			expanduser("~/.weechat/weechat.conf"),
-			expanduser("~/.weechat/irc.conf")
 		]
-		
+
 		# This is the path to where crunchbox stores all this plugin's cfg
 		# This is the path to where crunchbox stores all this plugin's cfg
 		self.class_name = self.__class__.__name__
@@ -20,8 +19,8 @@ class Weechat:
 
 	def load(self, profile_name, plugin_obj):
 		self.base.load(profile_name, self.class_name, plugin_obj)
-	
-		
+
+
 	# TODO script to cycle through weechat's FIFOs.
 	# send command to reload screen, maybe it'll apply changes on the fly
 
